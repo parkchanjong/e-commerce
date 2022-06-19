066a8418c37f50f3b43c7c150fca4f6b31fbc883
Park ChanJong <7000cj@gmail.com>
2022-06-19 16:09:22 +0900

    회원가입 로직 추가
    - 랜덤 문자열 합쳐 비밀번호 설정

--- src/main/java/ecommerce/shop/domain/user/User.java
+++ src/main/java/ecommerce/shop/domain/user/User.java
@@ -1,6 +1,12 @@
 package ecommerce.shop.domain.user;
 
 import ecommerce.shop.domain.BaseTimeEntity;
+import java.nio.charset.Charset;
+import java.util.Base64;
+import java.util.Random;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -37,13 +43,39 @@ public class User extends BaseTimeEntity {
     @Builder
     public User(String email, String password) {
         this.email = email;
-        this.password = password;
-        this.salt = "Asdfsdf";
+        String randomWord = randomWord();
+        this.salt = randomWord;
+
+        try {
+            this.password = encrypt(password + randomWord);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public User update(String name) {
         this.name = name;
 
         return this;
+    }
+
+    public String randomWord() {
+        byte[] array = new byte[7];
+        new Random().nextBytes(array);
+        return new String(array, Charset.forName("UTF-8"));
+    }
+
+    public String encrypt(String text) throws Exception {
+        String alg = "AES/CBC/PKCS5Padding";
+        final String key = "01234567890123456789012345678901";
+        String iv = key.substring(0, 16);
+
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
     }
 }
