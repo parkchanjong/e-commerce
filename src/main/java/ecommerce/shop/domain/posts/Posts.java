2ae83a96e0e8223c1808ed81f2fb31a7badb36b8
Park ChanJong <7000cj@gmail.com>
2022-06-25 16:36:56 +0900

    add posts domain

---
+++ src/main/java/ecommerce/shop/domain/posts/Posts.java
@@ -0,0 +1,41 @@
+package ecommerce.shop.domain.posts;
+
+import ecommerce.shop.domain.BaseTimeEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "POSTS")
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "POSTS_ID")
+    private Long id;
+
+    @Column(length = 500, nullable = false, name = "TITLE")
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false, name = "CONTENT")
+    private String content;
+
+    @Builder
+    public Posts(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
