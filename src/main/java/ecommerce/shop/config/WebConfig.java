3c7bf01a1ec441857f15168632d7d8bf1d9c667b
Park ChanJong <7000cj@gmail.com>
2022-07-05 17:24:39 +0900

    add logFilter

---
+++ src/main/java/ecommerce/shop/config/WebConfig.java
@@ -0,0 +1,22 @@
+package ecommerce.shop.config;
+
+import ecommerce.shop.web.filter.LogFilter;
+import javax.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
