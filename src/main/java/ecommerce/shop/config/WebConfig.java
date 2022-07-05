8d62462788e879d393bda9edfa4733e1aa8afc54
Park ChanJong <7000cj@gmail.com>
2022-07-05 20:14:36 +0900

    필터 스프링 인터셉터로 변경

--- src/main/java/ecommerce/shop/config/WebConfig.java
+++ src/main/java/ecommerce/shop/config/WebConfig.java
@@ -1,22 +1,17 @@
 package ecommerce.shop.config;
 
-import ecommerce.shop.web.filter.LogFilter;
-import javax.servlet.Filter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import ecommerce.shop.web.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean logFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        
-        filterRegistrationBean.setFilter(new LogFilter());
-        filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/*");
-
-        return filterRegistrationBean;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor()).order(1).addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
     }
+
 }
