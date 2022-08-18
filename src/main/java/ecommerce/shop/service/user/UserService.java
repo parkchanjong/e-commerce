09438fa597dccfb0ec3053fe9dbfd1e2fd51b67a
Park ChanJong <7000cj@gmail.com>
2022-08-18 23:23:56 +0900

    user duplication, login service exception 변경

--- src/main/java/ecommerce/shop/service/user/UserService.java
+++ src/main/java/ecommerce/shop/service/user/UserService.java
@@ -25,7 +25,7 @@ public class UserService {
     private void validateDuplicateUser(SignupDto signupDto) {
         Optional<User> findUser = userRepository.findByEmail(signupDto.getEmail());
         if (findUser.isPresent()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
         }
     }
 
@@ -36,7 +36,7 @@ public class UserService {
                 return user.getPassword()
                         .equals(user.encrypt(loginDto.getPassword() + user.getSalt()));
             } catch (Exception e) {
-                throw new IllegalStateException("아이디 또는 비밀번호가 맞지 않습니다.");
+                throw new IllegalArgumentException("아이디 또는 비밀번호가 맞지 않습니다.");
             }
         });
     }
