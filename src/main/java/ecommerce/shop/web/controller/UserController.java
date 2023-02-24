49468084a9d0b2205a5a92ca03d1b6eeb81ea1e2
Park ChanJong <7000cj@gmail.com>
2023-02-24 20:09:49 +0900

    controller parms modify

--- src/main/java/ecommerce/shop/web/controller/UserController.java
+++ src/main/java/ecommerce/shop/web/controller/UserController.java
@@ -1,8 +1,6 @@
 package ecommerce.shop.web.controller;
 
-import static ecommerce.shop.utils.HttpResponses.RESPONSE_BAD_REQUEST;
 import static ecommerce.shop.utils.HttpResponses.RESPONSE_OK;
-import static ecommerce.shop.utils.HttpResponses.RESPONSE_UNAUTHORIZED;
 
 import ecommerce.shop.domain.user.User;
 import ecommerce.shop.service.user.SessionConst;
@@ -16,54 +14,32 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
-@RequestMapping
 @RestController
+@RequestMapping("/users/")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<Void> signup(@Valid @ModelAttribute SignupDto signupDto,
-            BindingResult result) {
-
-        log.info("회원가입");
-        if (result.hasErrors()) {
-            return RESPONSE_BAD_REQUEST;
-        }
+    @PostMapping
+    public ResponseEntity<Void> signup(@Valid SignupDto signupDto) {
         userService.save(signupDto);
         return RESPONSE_OK;
     }
 
-    @GetMapping("/login")
-    public String addLoginForm(@ModelAttribute LoginDto loginDto) {
-        return "user/login";
-    }
-
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @ModelAttribute LoginDto loginDto,
-            BindingResult bindingResult, HttpServletRequest request) {
+    public ResponseEntity<Void> login(@Valid LoginDto loginDto,
+            HttpServletRequest request) {
 
-        if (bindingResult.hasErrors()) {
-            return RESPONSE_BAD_REQUEST;
-        }
         userService.login(loginDto);
         Optional<User> loginMember = userService.login(loginDto);
 
-        if (loginMember.isEmpty()) {
-            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
-            return RESPONSE_UNAUTHORIZED;
-        }
         HttpSession session = request.getSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
@@ -71,11 +47,7 @@ public class UserController {
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
+    public ResponseEntity<Void> logout() {
         return RESPONSE_OK;
     }
 }
