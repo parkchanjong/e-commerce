5d8d96f6265d933b41c16632d5315533112e4a7c
Park ChanJong <7000cj@gmail.com>
2023-03-13 21:31:45 +0900

    add async config

--- src/main/java/ecommerce/shop/ShopApplication.java
+++ src/main/java/ecommerce/shop/ShopApplication.java
@@ -2,12 +2,18 @@ package ecommerce.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAspectJAutoProxy
+@EnableAsync
+@EnableCaching
 @SpringBootApplication
 public class ShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
 
 }
--- src/main/java/ecommerce/shop/service/order/OrderService.java
+++ src/main/java/ecommerce/shop/service/order/OrderService.java
@@ -2,10 +2,12 @@ package ecommerce.shop.service.order;
 
 import ecommerce.shop.domain.order.OrdersRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class OrderService {
 
     private final OrdersRepository ordersRepository;
