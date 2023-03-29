6586d04c27588dd381e2e6cad11ac16ef7b220a9
Park ChanJong <7000cj@gmail.com>
2023-03-29 23:29:20 +0900

    #5 프로젝트 설정

--- src/main/java/ecommerce/shop/web/controller/ShopController.java
+++ src/main/java/ecommerce/shop/web/controller/ShopController.java
@@ -1,12 +1,9 @@
 package ecommerce.shop.web.controller;
 
 import ecommerce.shop.service.posts.PostsService;
-import ecommerce.shop.web.dto.posts.PostsListResponseDto;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,9 +14,5 @@ public class ShopController {
 
     private final PostsService postsService;
 
-    @GetMapping
-    public String list() {
-        return "shop/list";
-    }
-    
+
 }
