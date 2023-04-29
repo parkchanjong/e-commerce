3732394a6fe911b332b25ff18cbd6d048dca8c32
Park ChanJong <7000cj@gmail.com>
2023-04-29 18:27:55 +0900

    add DataSourceConstants

---
+++ src/main/java/ecommerce/shop/common/utils/constants/DataSourceConstants.java
@@ -0,0 +1,7 @@
+package ecommerce.shop.common.utils.constants;
+
+public class DataSourceConstants {
+
+    public static final String MASTER = "master";
+    public static final String SLAVE = "slave";
+}
