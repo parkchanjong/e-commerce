1cdf9b5206f6ed5a42575fa2d50d2f0d62ac3100
Park ChanJong <7000cj@gmail.com>
2023-05-03 15:48:29 +0900

    remove ordersRepository

--- src/main/java/ecommerce/shop/service/order/OrderService.java
+++ src/main/java/ecommerce/shop/service/order/OrderService.java
@@ -1,6 +1,5 @@
 package ecommerce.shop.service.order;
 
-import ecommerce.shop.domain.order.OrdersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,5 +9,4 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class OrderService {
 
-    private final OrdersRepository ordersRepository;
 }
