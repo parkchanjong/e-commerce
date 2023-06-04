c5f310cc0db61555ada96de136df378319127022
Park ChanJong <7000cj@gmail.com>
2023-06-04 21:48:38 +0900

    add checkEmailDuplicat

--- src/main/java/ecommerce/shop/web/controller/UserController.java
+++ src/main/java/ecommerce/shop/web/controller/UserController.java
@@ -3,6 +3,9 @@ package ecommerce.shop.web.controller;
 import ecommerce.shop.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,8 @@ public class UserController {
 
     private final UserService userService;
 
-
+    @GetMapping("/user-emails/{email}/exists")
+    public ResponseEntity<Boolean> checkEmailDuplicate(@PathVariable String email) {
+        return ResponseEntity.ok(userService.checkEmailDuplicate(email));
+    }
 }
